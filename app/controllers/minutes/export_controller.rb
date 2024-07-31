7a14a1a859bee232fa08b77ab0ea7cc5e71648ad
Kassy0220 <shinbokutaro0220@gmail.com>
2024-07-31 09:43:03 +0900

    GitHub Wikiにエクスポートする処理をクラスに切り出した

--- app/controllers/minutes/export_controller.rb
+++ app/controllers/minutes/export_controller.rb
@@ -1,20 +1,7 @@
 class Minutes::ExportController < ApplicationController
   def create
     minute = Minute.find(params[:minute_id])
-    Git.clone(ENV['GITHUB_WIKI_URL'], 'wiki_repositories') unless Dir.exist?('wiki_repositories')
-
-    repository_directory = Rails.root.join('wiki_repositories')
-    filepath = "#{repository_directory}/#{minute.title}.md"
-    File.open(filepath, 'w+') do |file|
-      file.puts minute.content # 実際のアプリでは、Markdownの末尾に空行があるため、writeでファイルに書き込む
-    end
-
-    git = Git.open('./wiki_repositories', log: Logger.new(STDOUT))
-    git.pull
-    git.add("#{minute.title}.md")
-    git.commit("#{minute.title}.mdを作成")
-    git.push('origin', 'master')
-    # TODO: 例外が発生した時の処理を書く
+    GithubWikiManager.export_minute(minute)
 
     redirect_to minutes_url, notice: "GitHub Wikiに議事録を反映させました"
   end
---
+++ app/models/github_wiki_manager.rb
@@ -0,0 +1,29 @@
+class GithubWikiManager
+  WORKING_DIRECTORY = Rails.root.join('wiki_repositories').freeze
+
+  def self.export_minute(minute)
+    new.export_minute(minute)
+  end
+
+  def initialize
+    @git = Dir.exist?(WORKING_DIRECTORY) ? Git.open(WORKING_DIRECTORY, log: Logger.new(STDOUT))
+                                         : Git.clone(ENV['GITHUB_WIKI_URL'], WORKING_DIRECTORY)
+  end
+
+  def export_minute(minute)
+    # TODO: 例外が発生した時の処理を書く
+    @git.pull
+    commit_minute(minute)
+    @git.push('origin', 'master')
+  end
+
+  def commit_minute(minute)
+    filepath = "#{WORKING_DIRECTORY}/#{minute.title}.md"
+    File.open(filepath, 'w+') do |file|
+      file.puts minute.content # 実際のアプリでは、Markdownの末尾に空行があるため、writeでファイルに書き込む
+    end
+
+    @git.add("#{minute.title}.md")
+    @git.commit("#{minute.title}.mdを作成")
+  end
+end
